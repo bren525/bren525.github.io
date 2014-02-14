34ced037c9aa17ecba65a0ee0ee9e5b649de829e
bren525 <bren525@gmail.com>
2014-02-14 12:27:15 -0500

    added some proj pics

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 $(document).ready(function(){
 	$("#exp1").hover(function(){
-		$(this).attr("src","http://dummyimage.com/110x110")
+		$(this).attr("src","http://dummyimage.com/1100x1100")
 	}, function(){
-		$(this).attr("src","http://dummyimage.com/100x100")
+		$(this).attr("src","GhostFiddle.jpg")
+	}).click(function(){
+			
+	});
+	$("#exp2").hover(function(){
+		$(this).attr("src","http://dummyimage.com/1100x1100")
+	}, function(){
+		$(this).attr("src","GA.jpg")
+	}).click(function(){
+			
+	});
+	$("#exp3").hover(function(){
+		$(this).attr("src","http://dummyimage.com/1100x1100")
+	}, function(){
+		$(this).attr("src","GhostFiddle.jpg")
 	}).click(function(){
 			
 	});
