3639ef7a01d24bed146fb79e1e5692a7069a6be9
bren525 <bren525@gmail.com>
2014-02-13 23:34:25 -0500

    Initial Lander

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$("#exp1").hover(function(){
+		$(this).attr("src","http://dummyimage.com/110x110")
+	}, function(){
+		$(this).attr("src","http://dummyimage.com/100x100")
+	}).click(function(){
+			
+	});
+
+});
