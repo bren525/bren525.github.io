6194a73e5fb0d01eeec415abe733320f2f735c46
Brendan <bren525@gmail.com>
2014-02-21 16:51:11 -0500

    Updated pics, added text

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 $(document).ready(function(){
 	$("#exp1").hover(function(){
-		$(this).attr("src","http://dummyimage.com/1100x1100")
+		$(this).attr("src","http://dummyimage.com/1100x1100");
+		$(".t1").toggleClass("hidden");
 	}, function(){
 		$(this).attr("src","GhostFiddle.jpg")
+		$(".t1").toggleClass("hidden");
 	}).click(function(){
 			
 	});
 	$("#exp2").hover(function(){
-		$(this).attr("src","http://dummyimage.com/1100x1100")
+		$(this).attr("src","http://dummyimage.com/1100x1100");
+		$(".t2").toggleClass("hidden");
 	}, function(){
-		$(this).attr("src","GA.jpg")
+		$(this).attr("src","GA.jpg");
+		$(".t2").toggleClass("hidden");
 	}).click(function(){
 			
 	});
 	$("#exp3").hover(function(){
-		$(this).attr("src","http://dummyimage.com/1100x1100")
+		$(this).attr("src","http://dummyimage.com/1100x1100");
+		$(".t3").toggleClass("hidden");
 	}, function(){
-		$(this).attr("src","GhostFiddle.jpg")
+		$(this).attr("src","GhostFiddle.jpg");
+		$(".t3").toggleClass("hidden");
 	}).click(function(){
 			
 	});
