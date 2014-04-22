6920d142ea4dcdef171534489325ee8f25fe3c1a
unknown <bcaporaletti@82WPFV1.olin.edu>
2014-04-22 16:51:23 -0400

    more stuffs

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
 $(document).ready(function(){
-	$("#exp1").hover(function(){
-		$(this).attr("src","http://dummyimage.com/1100x1100");
-		$(".t1").toggleClass("hidden");
+	var pics = {"b1":"GhostFiddle.jpg","a1":"http://dummyimage.com/1100x1100"
+				,"b2":"GA.jpg","a2":"http://dummyimage.com/1100x1100"
+				,"b3":"PipelinedZoom.png","a3":"http://dummyimage.com/1100x1100"}
+	$(".picLabel").hover(function(){
+		var num = $(this).attr("name");
+		$("#exp" + num).attr("src",pics["a"+num]);
+		$(".t"+num).toggleClass("hidden");
 	}, function(){
-		$(this).attr("src","GhostFiddle.jpg")
-		$(".t1").toggleClass("hidden");
+		var num = $(this).attr("name");
+		$("#exp" + num).attr("src",pics["b"+num])
+		$(".t"+num).toggleClass("hidden");
 	}).click(function(){
+		var num = $(this).attr("name");
+		if($("#des" +num).hasClass("hidden")){
+			$(".projDescription").css({"height":"0"})
+			.css({"width":"0"})
+			.addClass("hidden");
+			$("#des" +num).toggleClass("hidden")
+			.animate({"width":"100%"},100)
+			.animate({"height":"300px"},300);
+		}else{
+			$("#des" +num).css({"height":"0"})
+			.css({"width":"0"})
+			.toggleClass("hidden");
+		}
 			
 	});
-	$("#exp2").hover(function(){
-		$(this).attr("src","http://dummyimage.com/1100x1100");
-		$(".t2").toggleClass("hidden");
-	}, function(){
-		$(this).attr("src","GA.jpg");
-		$(".t2").toggleClass("hidden");
-	}).click(function(){
-			
-	});
-	$("#exp3").hover(function(){
-		$(this).attr("src","http://dummyimage.com/1100x1100");
-		$(".t3").toggleClass("hidden");
-	}, function(){
-		$(this).attr("src","PipelinedZoom.png");
-		$(".t3").toggleClass("hidden");
-	}).click(function(){
-			
+	$("#nav li").click(function(){
+		var loc = "#" + $(this).attr('name');
+		console.log(loc);
+		$(document.body).animate({'scrollTop':$(loc).offset().top}, 300); 
 	});
 
 });
